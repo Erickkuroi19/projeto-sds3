58d7f3f14402ebc162c3ae5c9c7a55357033719e
Rafael Santos <rafarag40@gmail.com>
2021-05-03 15:20:10 -0300

    Componentes saticos basicos

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,21 @@
+import Footer from "components/Footer";
+import NavBar from "components/Navbar";
+import DataTable from"components/DataTable";
+
 
 function App() {
   return (
-    <div >
-      <h1 className="text-primary">OLÁ MUNDO!</h1>
-    </div>
+    <>
+      <NavBar />
+      <div className="container">
+        <h1 className="text-primary">OLÁ MUNDO!</h1>
+        <DataTable />
+      </div>
+      
+      <Footer />
+    </>
   );
+
 }
 
 export default App;
